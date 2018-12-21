5b7916d5819962ff28b7403afc73ac426cf829bf
tails5555 <tails5555@naver.com>
2018-12-21 17:41:36 +0900

    Update ArrayList Example01 ArrayList 에서 삭제 메소드 구현 완료. 그리고 추가 기능과 삭제 기능 메소드를 같은 과정으로 작성하였음.

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 # Python Data Structure
 
-Python 을 이용한 자료구조 개념 복습 저장소
+Python 을 이용한 자료구조 개념 복습 저장소 입니다!
 
 ## Contexts
 
 1. ArrayList
+   1. Example01
+        - ArrayList 데이터 추가, 삭제 메소드 구현.
+        - 데이터 Create, Delete 에서 길이 최적화 기능도 구현.
+
+2. LinkedList
+3. Stack
+4. Queue
+5. Deque
+6. Heap
+7. PriorityQueue
+8. Binary Search Tree
+9.  Sorting
+10. Hash Table
 
 ## Author
 
--- array_list/example01.py
+++ array_list/example01.py
@@ -1,9 +1,11 @@
 class ArrayList :
+    # ArrayList 생성자
     def __init__(self, size) :
-        self.size = size
-        self.capacity = 0
-        self.data = [None] * size
+        self.size = size # 할당 ArrayList 길이
+        self.capacity = 0 # 실제 ArrayList 길이
+        self.data = [None] * size # ArrayList 데이터 목록
 
+    # Index 와 값으로 ArrayList 삽입 메소드
     def add_data(self, idx, value) :
         tmp_list = self.data
         
@@ -11,34 +13,112 @@ class ArrayList :
             print('ArrayList 의 인덱스를 다시 확인하세요. 인덱스는 {} 까지 가능합니다.'.format(self.capacity))
             return
 
-        if self.capacity >= self.size :
-            tmp_list = tmp_list + [None] * self.size
-            self.size *= 2
-        
         if len(tmp_list) > 0 :
             for i in range(self.capacity, idx, -1) :
                 tmp_list[i] = tmp_list[i - 1]
 
         tmp_list[idx] = value
+        
+        self.capacity += 1
+        if self.capacity >= self.size :
+            tmp_list = tmp_list + [None] * self.size
+            self.size *= 2
 
         self.data = tmp_list
-        self.capacity += 1
+        
 
+    # ArrayList 처음 자리에 값을 추가하는 메소드
     def add_first(self, value) :
         self.add_data(0, value)
     
+    # ArrayList 마지막 자리에 값을 추가하는 메소드
     def add_last(self, value) :
         self.add_data(self.capacity, value)
 
+    # Index 값으로 ArrayList 삭제 메소드
+    def remove_data(self, idx) :
+        tmp_list = self.data
+
+        if idx >= self.capacity :
+            print('ArrayList 의 인덱스를 다시 확인하세요. 인덱스는 {} 까지 가능합니다.'.format(self.capacity))
+            return
+        
+        tmp_list[idx] = None
+        
+        if len(tmp_list) > 0 :
+            for i in range(idx, self.capacity) :
+                if i < self.capacity - 1:
+                    tmp_list[i] = tmp_list[i + 1]
+                else :
+                    tmp_list[i] = None
+
+        self.capacity -= 1
+        if self.capacity <= self.size // 2 and self.size > 1 :
+            self.size //= 2
+            tmp_list = tmp_list[0:self.size]
+
+        self.data = tmp_list
+
+    # ArrayList 처음 자리 값을 삭제하는 메소드 
+    def remove_first(self) :
+        self.remove_data(0)
+    
+    # ArrayList 마지막 자리 값을 삭제하는 메소드 
+    def remove_last(self) :
+        self.remove_data(self.capacity - 1)
+
+    # ArrayList __str__ 메소드
     def __str__(self) :
-        return '{}'.format(self.data)
+        tmp_list = self.data
+        result_str = ''
+        for i in tmp_list :
+            if i != None :
+                result_str += '{} '.format(i)
+            else :
+                result_str += '{} '.format('-')
+
+        return '[ {}][Capacity : {}]'.format(result_str, self.capacity)
 
 array_list1 = ArrayList(5)
 
-array_list1.add_first(10) # [10]
-array_list1.add_last(20) # [10, 20]
-array_list1.add_first(30) # [30, 10, 20]
-array_list1.add_data(1, 40) # [30, 40, 10, 20]
-array_list1.add_data(2, 50) # [30, 40, 50, 10, 20]
-array_list1.add_data(1, 60) # 여기서 ArrayList 의 길이가 2배로 늘어납니다.
-print(array_list1) # [30, 60, 40, 50, 10, 20, None, None, None, None]
+array_list1.add_first(10)
+print(array_list1) # [ 10 - - - - ][Capacity : 1]
+
+array_list1.add_last(20)
+print(array_list1) # [ 10 20 - - - ][Capacity : 2]
+
+array_list1.add_first(30)
+print(array_list1) # [ 30 10 20 - - ][Capacity : 3]
+
+array_list1.add_data(1, 40)
+print(array_list1) # [ 30 40 10 20 - ][Capacity : 4]
+
+array_list1.add_data(2, 50) # 여기서 ArrayList 의 길이가 2배로 늘어납니다.
+print(array_list1) # [ 30 40 50 10 20 - - - - - ][Capacity : 5]
+
+array_list1.add_data(1, 60) 
+print(array_list1) # [ 30 60 40 50 10 20 - - - - ][Capacity : 6]
+
+array_list1.remove_first() # 여기서 ArrayList 의 길이가 1/2배로 줄어듭니다.
+print(array_list1) # [ 60 40 50 10 20 ][Capacity : 5]
+
+array_list1.remove_data(1) 
+print(array_list1) # [ 60 50 10 20 - ][Capacity : 4]
+
+array_list1.remove_last()
+print(array_list1) # [ 60 50 10 - - ][Capacity : 3]
+
+array_list1.remove_first() # 여기서 ArrayList 의 길이가 1/2배로 줄어듭니다.
+print(array_list1) # [ 50 10 ][Capacity : 2]
+
+array_list1.remove_data(1) # 여기서도 ArrayList 의 길이가 1/2배로 줄어듭니다.
+print(array_list1) # [ 50 ][Capacity : 1]
+
+array_list1.remove_first()
+print(array_list1) # [ - ][Capacity : 0]
+
+array_list1.add_data(0, 10) # 여기서 ArrayList 의 길이가 2배로 늘어납니다.
+print(array_list1) # [ 10 - ][Capacity : 1]
+
+array_list1.add_last(10) # 여기서도 ArrayList 의 길이가 2배로 늘어납니다.
+print(array_list1) # [ 10 10 - - ][Capacity : 2]
