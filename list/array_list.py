dcc997ccbed3cf9f56404cc8b078d08c66727247
tails5555 <tails5555@naver.com>
2018-12-25 02:27:08 +0900

    Update ArrayList Examples ArrayList 차집합, 병합 기능과 데이터 포함 여부와 인덱스 가져오는 기능 구현 완료.

--- list/array_list.py
+++ list/array_list.py
@@ -1,10 +1,38 @@
+# ArrayList Example
+# Writer : tails5555(Kang In Sung)
+# Written Date : 2018-12-25
+
 class ArrayList :
     # ArrayList 생성자
-    def __init__(self, size) :
-        self.size = size # 할당 ArrayList 길이
-        self.capacity = 0 # 실제 ArrayList 길이
-        self.data = [None] * size # ArrayList 데이터 목록
+    # Merge 연산을 위하여 파라미터 일부 조정
+    def __init__(self, size, capacity=0, data=None) :
+        # 할당 ArrayList 길이
+        self.size = size
+        # 실제 ArrayList 길이
+        self.capacity = capacity
+        # ArrayList 데이터 목록
+        self.data = [None] * size if data == None else data
 
+    # 각 멤버 변수 별 Setter 작성
+    def set_data(self, data) :
+        self.data = data
+
+    def set_size(self, size) :
+        self.size = size
+
+    def set_capacity(self, capacity) :
+        self.capacity = capacity
+    
+    # 각 멤버 변수 별 Getter 작성
+    def get_data(self) :
+        return self.data
+
+    def get_size(self) :
+        return self.size
+
+    def get_capacity(self) :
+        return self.capacity
+    
     # Index 와 값으로 ArrayList 삽입 메소드
     def add_data(self, idx, value) :
         tmp_list = self.data
@@ -25,7 +53,6 @@ class ArrayList :
             self.size *= 2
 
         self.data = tmp_list
-        
 
     # ArrayList 처음 자리에 값을 추가하는 메소드
     def add_first(self, value) :
@@ -67,6 +94,62 @@ class ArrayList :
     def remove_last(self) :
         self.remove_data(self.capacity - 1)
 
+    # ArrayList 에 데이터 포함을 확인하는 메소드
+    def contains(self, value) :
+        tmp_list = self.data
+        res = False
+        for i in tmp_list :
+            if i == value :
+                res = True
+                break
+                
+        return res
+
+    # ArrayList 에 데이터의 인덱스를 가져오는 메소드
+    def index_of(self, value) :
+        tmp_list = self.data
+        for (idx, k) in enumerate(tmp_list) :
+            if k == value :
+                return idx
+
+        return -1
+
+    # ArrayList __add__ 메소드. ArrayList 끼리 융합하기 위한 연산자 오버로딩
+    def __add__(self, another) :
+        tmp_size = self.size + another.get_size()
+        tmp_array_list = ArrayList(tmp_size)
+    
+        my_data = self.data
+        ano_data = another.data
+        
+        for k in my_data :
+            if k != None :
+                tmp_array_list.add_first(k)
+            else : 
+                break
+        
+        for k in ano_data :
+            if k != None :
+                tmp_array_list.add_first(k)
+            else : 
+                break
+
+        return tmp_array_list
+    
+    # ArrayList __sub__ 메소드. ArrayList 차집합(Minus) 을 위한 연산자 오버라이딩
+    def __sub__(self, another) :
+        my_data = self.data
+        ano_data = another.data
+
+        for (idx, k) in enumerate(my_data) :
+            for l in ano_data :
+                if l == None :
+                    break
+                if k == l :
+                    self.remove_data(self.index_of(l))
+
+        return self
+
     # ArrayList __str__ 메소드
     def __str__(self) :
         tmp_list = self.data
@@ -78,6 +161,8 @@ class ArrayList :
                 result_str += '{} '.format('-')
 
         return '[ {}][Capacity : {}]'.format(result_str, self.capacity)
+
+# Case 01. 데이터 추가 및 동적 길이 증가 확인
 
 array_list1 = ArrayList(5)
 
@@ -98,6 +183,8 @@ print(array_list1) # [ 30 40 50 10 20 - - - - - ][Capacity : 5]
 
 array_list1.add_data(1, 60) 
 print(array_list1) # [ 30 60 40 50 10 20 - - - - ][Capacity : 6]
+
+# Case 02. 데이터 삭제 및 동적 길이 감소 확인
 
 array_list1.remove_first() # 여기서 ArrayList 의 길이가 1/2배로 줄어듭니다.
 print(array_list1) # [ 60 40 50 10 20 ][Capacity : 5]
@@ -122,3 +209,45 @@ print(array_list1) # [ 10 - ][Capacity : 1]
 
 array_list1.add_last(10) # 여기서도 ArrayList 의 길이가 2배로 늘어납니다.
 print(array_list1) # [ 10 10 - - ][Capacity : 2]
+
+# Case 03. 데이터 포함 메소드, 데이터 인덱스 테스트
+
+array_list1.add_data(1, 20)
+print(array_list1.contains(10)) # True
+print(array_list1.index_of(20)) # 1
+print(array_list1.index_of(30)) # -1
+
+# Case 04. ArrayList Merge 연산 테스트
+
+array_list2 = ArrayList(2)
+array_list3 = ArrayList(3)
+
+array_list2.add_first(10)
+array_list2.add_first(20)
+
+print(array_list2) # [ 20 10 - - ][Capacity : 2] 길이가 2배로 늘어납니다.
+
+array_list3.add_last(5)
+array_list3.add_last(15)
+array_list3.add_last(25)
+
+print(array_list3) # [ 5 15 25 - - - ][Capacity : 3] 길이가 2배로 늘어납니다.
+
+print(array_list2 + array_list3) # [ 20 10 5 15 25 - - - - - ][Capacity : 5]
+
+# Case 05. ArrayList Minus 연산 테스트
+
+array_list4 = ArrayList(5)
+array_list5 = ArrayList(5)
+
+for k in range(10, 110, 10) :
+    array_list4.add_first(k)
+
+print(array_list4) # [ 100 90 80 70 60 50 40 30 20 10 - - - - - - - - - - ][Capacity : 10]
+
+for l in range(20, 220, 20) :
+    array_list5.add_last(l)
+
+print(array_list5) # [ 20 40 60 80 100 120 140 160 180 200 - - - - - - - - - - ][Capacity : 10]
+
+print(array_list4 - array_list5) # [ 90 70 50 30 10 ][Capacity : 5]
