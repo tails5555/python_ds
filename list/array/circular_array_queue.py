53dd675e6e5e2666e6ad4ed429c1843d362f13d9
tails5555 <tails5555@naver.com>
2018-12-31 23:28:13 +0900

    Update Circular Array Queue Examples empty, full 에 대한 조건을 다시 작성하였고, deQueue, enQueue 메소드 재작성.

--- list/array/circular_array_queue.py
+++ list/array/circular_array_queue.py
@@ -12,23 +12,27 @@ class CircularQueue :
         self._head = _head
         self._tail = _tail
 
-    def is_empty(self) :
-        return self._head == self._tail
-
     def is_full(self) :
-        return (self._head + 1) % self.size == self._tail
+        return (self._tail + 1) % self.size == self._head
+
+    def is_empty(self) :
+        return self._head == -1 and self._tail == -1
 
     def enQueue(self, value) :
         if self.is_full() :
             print('Circular Queue 의 공간이 꽉 찼습니다.')
             return
         else :
+            if self.is_empty() :
+                self._tail = 0
+                self._head = 0
+
             tmp_data = self.data
             tmp_tail = self._tail
             
+            tmp_data[tmp_tail] = value
             next_tail = (tmp_tail + 1) % self.size
-            tmp_data[next_tail] = value
-
+            
             self.data = tmp_data
             self._tail = next_tail
 
@@ -37,29 +41,54 @@ class CircularQueue :
             print('Circular Queue 의 공간이 비었습니다.')
             return
         else :
-            tmp_data = self.data
-            tmp_head = self._head
+            if self._head == self._tail :
+                print('Circular Queue 의 공간이 비었습니다.')
+                self._head = -1
+                self._tail = -1
+                return
 
-            next_head = (tmp_head + 1) % self.size
-            tmp_value = tmp_data[next_head]
+            else :
+                tmp_data = self.data
+                tmp_head = self._head
+                
+                tmp_value = tmp_data[tmp_head]
+                tmp_data[tmp_head] = None
+                next_head = (tmp_head + 1) % self.size
+                
+                self.data = tmp_data
+                self._head = next_head
 
-            tmp_data[next_head] = None
-            
-            self.data = tmp_data
-            self._head = next_head
-
-            return tmp_value
+                return tmp_value
 
     def __str__(self) :
         tmp_idx = self._head
+        tmp_end = self._tail
         tmp_data = self.data
         result_str = '[ '
         result_cap = 0
 
-        while tmp_idx != self._tail :
+        while tmp_idx != tmp_end :
+            result_str += '{} '.format(tmp_data[tmp_idx])
             result_cap += 1
             tmp_idx = (tmp_idx + 1) % self.size
-            result_str += '{} '.format(tmp_data[tmp_idx])
             
         result_str += '] [Capacity : {}]'.format(result_cap)
         return result_str
+
+circular_queue1 = CircularQueue(10)
+print(circular_queue1)
+
+for k in range(5, 50, 5) :
+    circular_queue1.enQueue(k)
+    print(circular_queue1)
+
+for k in range(1, 5, 1) :
+    print(circular_queue1.deQueue())
+    print(circular_queue1)
+
+print(circular_queue1.deQueue())
+print(circular_queue1)
+
+for k in range(5, 50, 5) :
+    circular_queue1.enQueue(k)
+    print(circular_queue1)
